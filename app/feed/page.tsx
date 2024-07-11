d5677bedea433acd65e575a7d99532dedbcfa955
gabyballester <gaby.ballester@gmail.com>
2024-07-12 01:55:30 +0200

    Add form to new-post and error component

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,9 +1,9 @@
 import { Posts } from "@/components";
 import { getPosts } from "@/database";
 
-
 export default async function FeedPage() {
   const posts = await getPosts();
+
   return (
     <>
       <h1>All posts by all users</h1>

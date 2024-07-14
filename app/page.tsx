1008da254a2cefe8f0c051ae4a98203f6e5e925d
gabyballester <gaby.ballester@gmail.com>
2024-07-14 20:28:36 +0200

    Fix the unescaped single quotes

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ import { getPosts } from "@/services";
 export default async function Home() {
   const latestPosts = await getPosts(3);
 
-  return (
-    <>
-      <h1>Welcome back!</h1>
-      <p>Here's what you might've missed.</p>
-      <section id="latest-posts">
-        <Suspense fallback={<p>Loading recent posts...</p>}>
-          <PostList posts={latestPosts} />
-        </Suspense>
-      </section>
-    </>
-  );
+   return (
+     <>
+       <h1>Welcome back!</h1>
+       <p>Here&apos;s what you might&apos;ve missed.</p>
+       <section id="latest-posts">
+         <Suspense fallback={<p>Loading recent posts...</p>}>
+           <PostList posts={latestPosts} />
+         </Suspense>
+       </section>
+     </>
+   );
 }
